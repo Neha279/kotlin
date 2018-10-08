261e24e887c0aa0c39f0a182c34c9b1ffd7f4279
Neha279 <nehaneha.rai@gmail.com>
2018-10-08 10:45:00 +0800

    test

---
+++ src/test/circleTest.kt
@@ -0,0 +1,31 @@
+package game
+
+
+import game.Circle;
+import game.Dice;
+
+
+class circleTest {
+
+    var circle = Circle();
+
+    //@Test
+    fun testCircle(){
+
+        //Assertions.assertEquals(circle.createFields(10));
+    }
+
+    //@Test
+    fun testCircleUpdate(){
+
+       // Assertions.assertEquals( circle.updateFields(6));
+    }
+
+    //@Test
+    fun testshowNumber(){
+        // Assertions.assertEquals( circle.getCurrentFeilds());
+    }
+}
+
+
+
