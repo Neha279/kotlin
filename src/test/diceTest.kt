de75ea163f05a4f5ad30aad7c1732a6eacdca049
Neha279 <nehaneha.rai@gmail.com>
2018-10-08 10:44:29 +0800

    test

---
+++ src/test/diceTest.kt
@@ -0,0 +1,21 @@
+
+
+
+import game.Circle;
+import game.Dice;
+//import org.junit.Test
+//import kotlin.test.*
+class diceTest {
+
+    val dice = Dice()
+    val circle = Circle();
+   // @Test
+    fun testDice(){
+        //Assertions.assertEquals(dice.init("10", circle));
+    }
+
+    //@Test
+    fun testRoll(){
+        //Assertions.assertEquals( dice.roll());
+    }
+}
